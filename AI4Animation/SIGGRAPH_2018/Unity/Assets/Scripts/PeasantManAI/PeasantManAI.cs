cc21fcf03f449b38e8f7dd1cdb1a036ff49fdcd2
Gabriel <rexesgames18@gmail.com>
2021-02-25 08:32:39 +0000

    setup npc enemy

---
+++ AI4Animation/SIGGRAPH_2018/Unity/Assets/Scripts/PeasantManAI/PeasantManAI.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FiniteStateMachine;
+using UnityEngine.AI;
+
+public class PeasantManAI : MonoBehaviour
+{
+    [SerializeField]private States state = States.CHASE;
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Animator anim;
+    [SerializeField] private GameObject player;
+    [SerializeField]private float maxDistanceToTarget = 2f;
+    private bool IsAttacking = false;
+    
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        switch (state)
+        {
+            case States.CHASE:
+                Chase();
+                break;
+            case States.ATTACK:
+                Attack();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Chase()
+    {
+        SetDestination(player.transform.position);
+
+        float distanceToTarget = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+        if (distanceToTarget <= 2f)
+        {
+            agent.isStopped = true;
+            SetState(States.ATTACK);
+        }
+    }
+
+    private void Attack()
+    {
+        float distanceToTarget = Vector3.Distance(gameObject.transform.position, player.transform.position);
+        IsAttacking = true;
+        anim.SetBool("IsAttacking", true);
+
+        if (distanceToTarget > 2f)
+        {
+            SetState(States.CHASE);
+        }
+    }
+
+    private void SetDestination(Vector3 targetVector)
+    {
+        agent.SetDestination(targetVector);
+    }
+
+
+    private void SetState(States newState)
+    {
+        state = newState;
+    }
+}
+
+namespace FiniteStateMachine
+{
+    public enum States
+    {
+        CHASE,
+        ATTACK
+    }
+}
+
+
