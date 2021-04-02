bf27055b5186e5dc001898d59a4effc23e28ff2b
Gabriel Alabi <rexesgames18@gmail.com>
2021-04-02 18:15:24 +0100

    add waypoint system

---
+++ AI4Animation/SIGGRAPH_2018/Unity/Assets/Scripts/WayPoint/WaypointSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointSwitcher : MonoBehaviour
+{
+    [SerializeField] private float sphereRadius;
+    [SerializeField] private WayPointTrail wayPointTrail;
+    [SerializeField] private float distance;
+
+    private void Update()
+    {
+        distance = Vector3.Distance(transform.position, Player.Instance.transform.position);
+
+        if (distance <= sphereRadius)
+        {
+            wayPointTrail.WayPointIndex++;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(transform.position, sphereRadius);
+    }
+}
