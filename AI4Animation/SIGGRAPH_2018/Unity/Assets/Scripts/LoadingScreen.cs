d4cd84982a00cd5432b20716bed74cb020f81f8d
Gabriel Alabi <rexesgames18@gmail.com>
2021-04-09 08:13:01 +0100

    Added Loading Screen

---
+++ AI4Animation/SIGGRAPH_2018/Unity/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField] private float waitTime;
+    [SerializeField] private float maxWaitTime = 2f;
+
+    private void Update()
+    {
+        waitTime += Time.deltaTime;
+
+        if (waitTime >= maxWaitTime)
+        {
+            waitTime = 0f;
+            gameObject.SetActive(false);
+        }
+    }
+}
