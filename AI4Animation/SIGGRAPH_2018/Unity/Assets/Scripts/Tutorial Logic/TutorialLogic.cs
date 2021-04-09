23ae1a08f2caa7c882888a7fccfa3da1da29f838
Gabriel Alabi <rexesgames18@gmail.com>
2021-04-09 08:36:43 +0100

    Applied standard wolf and added standard wolf prefab

--- AI4Animation/SIGGRAPH_2018/Unity/Assets/Scripts/Tutorial Logic/TutorialLogic.cs
+++ AI4Animation/SIGGRAPH_2018/Unity/Assets/Scripts/Tutorial Logic/TutorialLogic.cs
@@ -4,6 +4,12 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum SceneName
+{
+    ML,
+    STANDARD
+}
+
 public class TutorialLogic : MonoBehaviour
 {
     [SerializeField] private bool playTutorialFirst = true;
@@ -22,6 +28,7 @@ public class TutorialLogic : MonoBehaviour
     private bool isQPressed = false;
     private bool isEPressed = false;
     private bool isLShiftPressed = false;
+    public SceneName sceneName;
     
 
 
@@ -29,79 +36,145 @@ public class TutorialLogic : MonoBehaviour
     {
         if (playTutorialFirst)
             isInTutorial = true;
+
+        if (sceneName == SceneName.STANDARD)
+        {
+            isAPressed = true;
+            isDPressed = true;
+        }
     }
 
 
     private void Update()
     {
-        if (isInTutorial)
+        switch (sceneName)
         {
-            if (isWPressed == false)
-            {
-                if (Input.GetKeyDown(KeyCode.W))
+            case SceneName.ML:
+                if (isInTutorial)
                 {
-                    wKey.GetComponent<TextMeshProUGUI>().color = Color.green;
-                    isWPressed = true;
-                }
-            }
+                    if (isWPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.W))
+                        {
+                            wKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isWPressed = true;
+                        }
+                    }
 
-            if (isSPressed == false)
-            {
-                if (Input.GetKeyDown(KeyCode.S))
+                    if (isSPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.S))
+                        {
+                            sKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isSPressed = true;
+                        }
+                    }
+
+                    if (isAPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.A))
+                        {
+                            aKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isAPressed = true;
+                        }
+                    }
+
+                    if (isDPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.D))
+                        {
+                            dKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isDPressed = true;
+                        }
+                    }
+
+                    if (isQPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.Q))
+                        {
+                            qKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isQPressed = true;
+                        }
+                    }
+
+                    if (isEPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.E))
+                        {
+                            eKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isEPressed = true;
+                        }
+                    }
+
+                    if (isLShiftPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.LeftShift))
+                        {
+                            lShiftKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isLShiftPressed = true;
+                        }
+                    }
+
+                    if (isWPressed && isSPressed && isAPressed && isDPressed && isQPressed && isEPressed && isLShiftPressed)
+                        SceneManager.LoadSceneAsync("Demo ML Wolf");
+
+                }
+                break;
+            case SceneName.STANDARD:
+                if (isInTutorial)
                 {
-                    sKey.GetComponent<TextMeshProUGUI>().color = Color.green;
-                    isSPressed = true;
-                }
-            }
+                    if (isWPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.W))
+                        {
+                            wKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isWPressed = true;
+                        }
+                    }
 
-            if (isAPressed == false)
-            {
-                if (Input.GetKeyDown(KeyCode.A))
-                {
-                    aKey.GetComponent<TextMeshProUGUI>().color = Color.green;
-                    isAPressed = true;
-                }
-            }
-            
-            if (isDPressed == false)
-            {
-                if (Input.GetKeyDown(KeyCode.D))
-                {
-                    dKey.GetComponent<TextMeshProUGUI>().color = Color.green;
-                    isDPressed = true;
-                }
-            }
+                    if (isSPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.S))
+                        {
+                            sKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isSPressed = true;
+                        }
+                    }
 
-            if (isQPressed == false)
-            {
-                if (Input.GetKeyDown(KeyCode.Q))
-                {
-                    qKey.GetComponent<TextMeshProUGUI>().color = Color.green;
-                    isQPressed = true;
+                    if (isQPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.Q))
+                        {
+                            qKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isQPressed = true;
+                        }
+                    }
+
+                    if (isEPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.E))
+                        {
+                            eKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isEPressed = true;
+                        }
+                    }
+
+                    if (isLShiftPressed == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.LeftShift))
+                        {
+                            lShiftKey.GetComponent<TextMeshProUGUI>().color = Color.green;
+                            isLShiftPressed = true;
+                        }
+                    }
+
+                    if (isWPressed && isSPressed && isAPressed && isDPressed && isQPressed && isEPressed && isLShiftPressed)
+                        SceneManager.LoadSceneAsync("Demo Standard Wolf");
                 }
-            }
-
-            if (isEPressed == false)
-            {
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    eKey.GetComponent<TextMeshProUGUI>().color = Color.green;
-                    isEPressed = true;
-                }
-            }
-
-            if (isLShiftPressed == false)
-            {
-                if (Input.GetKeyDown(KeyCode.LeftShift))
-                {
-                    lShiftKey.GetComponent<TextMeshProUGUI>().color = Color.green;
-                    isLShiftPressed = true;
-                }
-            }
-
-            if (isWPressed && isSPressed && isAPressed && isDPressed && isQPressed && isEPressed && isLShiftPressed)
-                SceneManager.LoadSceneAsync("Demo");
-
+                break;
+            default:
+                break;
         }
+       
     }
 }
